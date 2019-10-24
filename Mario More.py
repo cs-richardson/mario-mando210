55bdf70ebffef7d26fd3e34919a6cf7fafa082a2
Miki Ando <MikiAndo@Miki.local>
2019-10-24 08:33:58 +0700

    mario

---
+++ Mario More.py
@@ -0,0 +1,21 @@
+'''
+This program prints out a double half-pyramid of a given height from the user
+Miki Ando
+'''
+
+#get the height of the double half-pyramid from the user
+height = input("Height: ")
+
+#If the values are not a digit or is not in the range between 0 and 23,
+#the program asks the user to re-enter the height
+while not(height.isdigit()) or int(height) < 0 or int(height) > 23:
+        height = input("Retry: ")
+
+#height is turned into an integer 
+height = int(height)
+
+#prints out the double half-pyramid  
+for i in range(1, height + 1):
+    block = ("#" * i) + " " + ("#" * i)
+    space = " " * (height - (i))
+    print(space + block)
